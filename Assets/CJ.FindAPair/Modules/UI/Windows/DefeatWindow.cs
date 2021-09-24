854befa1fb75ddb442e767cee3ccf4e9f1e91daf
tsargorox404 <ledlampsave@gmail.com>
2021-09-24 19:54:39 +0300

    work on defeat notification

--- Assets/CJ.FindAPair/Modules/UI/Windows/DefeatWindow.cs
+++ Assets/CJ.FindAPair/Modules/UI/Windows/DefeatWindow.cs
@@ -1,4 +1,5 @@
 using CJ.FindAPair.Modules.CoreGames;
+using CJ.FindAPair.Modules.CoreGames.SpecialCards;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,20 +11,32 @@ public class DefeatWindow : Window
     [SerializeField] private Button _exitButton;
     [SerializeField] private Button _adsButton;
     [SerializeField] private TextMeshProUGUI _currentLevelText;
+    [SerializeField] private TextMeshProUGUI _defeatNotificationText;
+    [SerializeField] private string _timeIsOverMessage;
+    [SerializeField] private string _livesAreOverMessage;
+    [SerializeField] private string _bombDetonatedMessage;
+    [SerializeField] private string _fortuneCardRealisedMessage;
 
     private LevelCreator _levelCreator;
     private GameWatcher _gameWatcher;
+    private BombCard _bombCard;
+    private FortuneCard _fortuneCard;
+    private string _currentMessage;
 
     [Inject]
-    public void Construct(LevelCreator levelCreator, GameWatcher gameWatcher)
+    public void Construct(LevelCreator levelCreator, GameWatcher gameWatcher,
+        BombCard bombCard, FortuneCard fortuneCard)
     {
         _levelCreator = levelCreator;
         _gameWatcher = gameWatcher;
+        _bombCard = bombCard;
+        _fortuneCard = fortuneCard;
     }
 
     protected override void Init()
     {
         _gameWatcher.ThereWasADefeat += Open;
+
         _restartButton.onClick.AddListener(OnRestartButtonClick);
         _exitButton.onClick.AddListener(OnExitButtonClick);
         _adsButton.onClick.AddListener(OnAdsButtonClick);
@@ -31,7 +44,21 @@ public class DefeatWindow : Window
 
     protected override void OnOpen()
     {
+        _gameWatcher.TimeIsOut += TimeIsOver;
+        _gameWatcher.LivesIsOut += LivesAreOver;
+        _bombCard.BombDetonate += BombDetonated;
+        _fortuneCard.CardRealised += FortuneCardRealised;
+
         _currentLevelText.SetText(_levelCreator.LevelConfig.LevelNumber.ToString());
+        _defeatNotificationText.SetText(_currentMessage);
+    }
+
+    protected override void OnClose()
+    {
+        _gameWatcher.TimeIsOut -= TimeIsOver;
+        _gameWatcher.LivesIsOut -= LivesAreOver;
+        _bombCard.BombDetonate -= BombDetonated;
+        _fortuneCard.CardRealised -= FortuneCardRealised;
     }
 
     private void OnRestartButtonClick()
@@ -48,5 +75,25 @@ public class DefeatWindow : Window
 
     private void OnAdsButtonClick()
     {
+    }
+
+    private void TimeIsOver()
+    {
+        _currentMessage = _timeIsOverMessage;
+    }
+
+    private void LivesAreOver()
+    {
+        _currentMessage = _livesAreOverMessage;
+    }
+
+    private void BombDetonated()
+    {
+        _currentMessage = _bombDetonatedMessage;
+    }
+
+    private void FortuneCardRealised()
+    {
+        _currentMessage = _fortuneCardRealisedMessage;
     }
 }
