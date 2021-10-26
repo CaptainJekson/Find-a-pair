b485455af8aebc8b2edb85ee95ac521f0816991b
EvgeniySkvortsov <creeger7772@gmail.com>
2021-10-27 00:48:16 +0300

    test db connection

--- Assets/CJ.FindAPair/Scenes/TestConnection.cs
+++ Assets/CJ.FindAPair/Scenes/TestConnection.cs
@@ -1,26 +1,57 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestConnection : MonoBehaviour
 {
-    private string _path = "https://c91780.hostru10.fornex.host/orange-games777.ru/Find-a-pair/";
+    [SerializeField] private Toggle _isRegToggle;
+    [SerializeField] private TMP_InputField _nameText;
+    [SerializeField] private TMP_InputField _passwordText;
+    [SerializeField] private TextMeshProUGUI _logText;
+    [SerializeField] private Button _button;
+    
+    private string _registerPath = "https://c91780.hostru10.fornex.host/orange-games777.ru/Find-a-pair/register.php";
+    private string _loginPath = "https://c91780.hostru10.fornex.host/orange-games777.ru/Find-a-pair/login.php";
     
     private void Start()
     {
-        StartCoroutine(Send());
+        _button.onClick.AddListener(() =>
+        {
+            if(_isRegToggle.isOn)
+                StartCoroutine(RegisterUser());
+            else
+                StartCoroutine(LoginUser());
+        });
     }
 
-    private IEnumerator Send()
+    private IEnumerator RegisterUser()
     {
         var form = new WWWForm();
-        form.AddField("HelloServer", "привет сервер!");
-        var www = new WWW(_path, form);    
+        form.AddField("Name", _nameText.text);
+        form.AddField("Password", _passwordText.text);
+        var www = new WWW(_registerPath, form);    
         yield return www;
         if (www.error != null)
         {
-            Debug.LogError("Ошибка: " + www.error);
+            _logText.text = "Ошибка: " + www.error;
             yield break;
         }
-        Debug.Log("Сервер ответил: " + www.text);
+        _logText.text = "Сервер ответил: " + www.text;
+    }
+    
+    private IEnumerator LoginUser()
+    {
+        var form = new WWWForm();
+        form.AddField("User", _nameText.text);
+        form.AddField("Password", _passwordText.text);
+        var www = new WWW(_loginPath, form);    
+        yield return www;
+        if (www.error != null)
+        {
+            _logText.text = "Ошибка: " + www.error;
+            yield break;
+        }
+        _logText.text = "Сервер ответил: " + www.text;
     }
 }
