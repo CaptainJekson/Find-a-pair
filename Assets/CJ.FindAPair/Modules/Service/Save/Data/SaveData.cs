4dbe2282715b95509795c0c1ec34cf8b3f905cd0
EvgeniySkvortsov <creeger7772@gmail.com>
2021-11-05 00:46:32 +0300

    connect test methods

--- Assets/CJ.FindAPair/Modules/Service/Save/Data/SaveData.cs
+++ Assets/CJ.FindAPair/Modules/Service/Save/Data/SaveData.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 [Serializable]
 public class SaveData
 {
+    public int UserId;
     public int CurrentLevel;
     public ItemsData ItemsData;
     public ThemesData ThemesData;
