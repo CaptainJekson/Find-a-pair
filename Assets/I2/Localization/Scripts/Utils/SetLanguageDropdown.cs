ea23581699b64e3f6a56df66f929eeab25b85521
tsargorox404 <ledlampsave@gmail.com>
2021-11-19 16:06:23 +0300

    rework language dropdown

--- Assets/I2/Localization/Scripts/Utils/SetLanguageDropdown.cs
+++ Assets/I2/Localization/Scripts/Utils/SetLanguageDropdown.cs
@@ -1,12 +1,15 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace I2.Loc
 {
 	[AddComponentMenu("I2/Localization/SetLanguage Dropdown")]
-	public class SetLanguageDropdown : MonoBehaviour 
+	public class SetLanguageDropdown : MonoBehaviour
 	{
+		[SerializeField] private List<Sprite> _itemImages;
+		
         #if UNITY_5_2 || UNITY_5_3 || UNITY_5_4_OR_NEWER
         void OnEnable()
 		{
@@ -20,15 +23,31 @@ namespace I2.Loc
 
 			// Fill the dropdown elements
 			dropdown.ClearOptions();
-			dropdown.AddOptions( languages );
+			dropdown.AddOptions(SetOptions(languages));
 
 			dropdown.value = languages.IndexOf( currentLanguage );
 			dropdown.onValueChanged.RemoveListener( OnValueChanged );
 			dropdown.onValueChanged.AddListener( OnValueChanged );
 		}
 
-		
-		void OnValueChanged( int index )
+        private List<TMP_Dropdown.OptionData> SetOptions(List<string> languages)
+        {
+	        List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+	        
+	        for (int i = 0; i < _itemImages.Count; i++)
+	        {
+		        TMP_Dropdown.OptionData optionData = new TMP_Dropdown.OptionData();
+
+		        optionData.text = languages[i];
+		        optionData.image = _itemImages[i];
+				
+		        options.Add(optionData);
+	        }
+
+	        return options;
+        }
+
+        void OnValueChanged( int index )
 		{
 			var dropdown = GetComponent<TMP_Dropdown>();
 			if (index<0)
