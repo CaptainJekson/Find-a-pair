9fc69ebf06f39dde1256a7028f90889f47010302
tsargorox404 <ledlampsave@gmail.com>
2021-11-26 21:59:41 +0300

    mistakes with window

--- Assets/CJ.FindAPair/Modules/UI/Windows/ConfirmRestartWindow.cs
+++ Assets/CJ.FindAPair/Modules/UI/Windows/ConfirmRestartWindow.cs
@@ -1,4 +1,5 @@
 using CJ.FindAPair.Modules.CoreGames;
+using CJ.FindAPair.Modules.UI.Installer;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -10,21 +11,35 @@ namespace CJ.FindAPair.Modules.UI.Windows
         [SerializeField] private Button _restartButton;
 
         private LevelCreator _levelCreator;
+        private EnergyCooldownHandler _energyCooldownHandler;
+        private UIRoot _uiRoot;
+        private ISaver _gameSaver;
 
         [Inject]
-        public void Construct(LevelCreator levelCreator)
+        public void Construct(LevelCreator levelCreator, EnergyCooldownHandler energyCooldownHandler, UIRoot uiRoot, ISaver gameSaver)
         {
             _levelCreator = levelCreator;
+            _energyCooldownHandler = energyCooldownHandler;
+            _uiRoot = uiRoot;
+            _gameSaver = gameSaver;
         }
 
-        protected override void Init()
+        protected override void OnOpen()
         {
             _restartButton.onClick.AddListener(OnRestartButtonClick);
+        }
+
+        protected override void OnClose()
+        {
+            _restartButton.onClick.RemoveListener(OnRestartButtonClick);
         }
 
         private void OnRestartButtonClick()
         {
             _levelCreator.RestartLevel();
+            _energyCooldownHandler.TryDecreaseScore();
+            _uiRoot.OpenWindow<GameInterfaceWindow>();
+            Close();
         }
     }
 }
--- Assets/CJ.FindAPair/Modules/UI/Windows/GameMenuWindow.cs
+++ Assets/CJ.FindAPair/Modules/UI/Windows/GameMenuWindow.cs
@@ -1,4 +1,6 @@
 using CJ.FindAPair.Modules.CoreGames;
+using CJ.FindAPair.Modules.UI.Installer;
+using CJ.FindAPair.Modules.UI.Windows;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,25 +9,28 @@ using Zenject;
 public class GameMenuWindow : Window
 {
     [SerializeField] private TextMeshProUGUI _currentLevelText;
-    
     [SerializeField] private Button _exitButton;
     [SerializeField] private Button _restartButton;
-    
+
     private LevelCreator _levelCreator;
     private EnergyCooldownHandler _energyCooldownHandler;
-    
+    private UIRoot _uiRoot;
+    private ISaver _gameSaver;
+
     [Inject]
-    public void Construct(LevelCreator levelCreator, EnergyCooldownHandler energyCooldownHandler)
+    public void Construct(LevelCreator levelCreator, EnergyCooldownHandler energyCooldownHandler, UIRoot uiRoot, ISaver gameSaver)
     {
         _levelCreator = levelCreator;
         _energyCooldownHandler = energyCooldownHandler;
+        _uiRoot = uiRoot;
+        _gameSaver = gameSaver;
     }
 
     protected override void OnOpen()
     {
         _exitButton.onClick.AddListener(_energyCooldownHandler.TryDecreaseScore);
-        _restartButton.onClick.AddListener(_energyCooldownHandler.TryDecreaseScore);
-        
+        _restartButton.onClick.AddListener(OnRestartButtonClick);
+
         Time.timeScale = 0.0f;
         _currentLevelText.SetText(_levelCreator.LevelConfig.LevelNumber.ToString());
     }
@@ -33,8 +38,24 @@ public class GameMenuWindow : Window
     protected override void OnClose()
     {
         _exitButton.onClick.RemoveListener(_energyCooldownHandler.TryDecreaseScore);
-        _restartButton.onClick.RemoveListener(_energyCooldownHandler.TryDecreaseScore);
-        
+        _restartButton.onClick.RemoveListener(OnRestartButtonClick);
+
         Time.timeScale = 1.0f;
+    }
+    
+    private void OnRestartButtonClick()
+    {
+        var saveData = _gameSaver.LoadData();
+            
+        if (saveData.ItemsData.Energy <= 0)
+        {
+            _uiRoot.OpenWindow<EnergyBoostOfferWindow>();  
+        }
+        else
+        {
+            _uiRoot.OpenWindow<ConfirmRestartWindow>();
+            _uiRoot.CloseWindow<GameBlockWindow>();
+            Close();
+        }
     }
 }
