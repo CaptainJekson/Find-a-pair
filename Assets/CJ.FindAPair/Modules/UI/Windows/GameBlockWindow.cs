4352d393b75ead8e8e748fa087d5e2df3aa1fcb0
tsargorox404 <ledlampsave@gmail.com>
2021-12-08 12:49:37 +0300

    refix

--- Assets/CJ.FindAPair/Modules/UI/Windows/GameBlockWindow.cs
+++ Assets/CJ.FindAPair/Modules/UI/Windows/GameBlockWindow.cs
@@ -1,5 +1,4 @@
 ﻿using CJ.FindAPair.Modules.CoreGames;
-using CJ.FindAPair.Modules.UI.Installer;
 using Zenject;
 
 namespace CJ.FindAPair.Modules.UI.Windows
@@ -7,13 +6,11 @@ namespace CJ.FindAPair.Modules.UI.Windows
     public class GameBlockWindow : Window
     {
         private LevelCreator _levelCreator;
-        private UIRoot _uiRoot;
-        
+
         [Inject]
-        public void Construct(LevelCreator levelCreator, UIRoot uiRoot)
+        public void Construct(LevelCreator levelCreator)
         {
             _levelCreator = levelCreator;
-            _uiRoot = uiRoot;
         }
         
         protected override void OnOpen()
@@ -21,17 +18,6 @@ namespace CJ.FindAPair.Modules.UI.Windows
             foreach (var card in _levelCreator.Cards)
             {
                 card.DisableInteractable();
-            }
-        }
-
-        protected override void OnClose()
-        {
-            if (_uiRoot.GetWindow<FullBlockerWindow>().gameObject.activeSelf == false)
-            {
-                foreach (var card in _levelCreator.Cards)
-                {
-                    card.EnableInteractable();
-                }
             }
         }
     }
