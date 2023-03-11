aa01f7269cca45db8488383be3b7647475e7e880
Evgeniy Skvortsov <e.skvortsov@chillgaming.net>
2023-03-12 00:49:39 +0300

    first features create

---
+++ Assets/Code/Features/LevelCreateFeature/Components/LevelCreate.cs
@@ -0,0 +1,10 @@
+﻿using CJ.FindAPair.Modules.CoreGames.Configs;
+using Scellecs.Morpeh;
+
+namespace Code.Features.LevelCreateFeature.Components
+{
+    public struct LevelCreate : IComponent
+    {
+        public LevelConfig level;
+    }
+}
---
+++ Assets/Code/Features/LevelCreateFeature/Systems/LevelCreateSystem.cs
@@ -0,0 +1,20 @@
+﻿using Code.Features.LevelCreateFeature.Components;
+using Code.GlobalUtils;
+using Scellecs.Morpeh;
+
+namespace Code.Features.LevelCreateFeature.Systems
+{
+    public class LevelCreateSystem : SimpleSystem<LevelCreate>, ISystem
+    {
+        [Injectable] private Stash<LevelCreate> _levelCreate;
+        
+        public void OnUpdate(float deltaTime)
+        {
+            foreach (var entity in _filter)
+            {
+                
+                _levelCreate.Remove(entity);
+            }
+        }
+    }
+}
